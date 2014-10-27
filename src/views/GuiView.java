0523461f464f79aa4446e98957ac58a27a4fd87c
reycs <rubenhaasjes@gmail.com>
2014-10-27 10:16:06 +0100

    Pi JFrame test

---
+++ src/views/GuiView.java
@@ -0,0 +1,16 @@
+package views;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class GuiView {
+
+	/* Demo to test GUI */
+	public static void main(String[] args) {
+		JFrame window = new JFrame("ARChess");
+		window.setSize(new Dimension(800, 600));
+		window.setVisible(true);
+	}
+
+}
