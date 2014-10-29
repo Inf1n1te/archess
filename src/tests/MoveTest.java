3b05462766856e8ae1c9a36b49f0d14274fae8f1
Inf1n1te <timkerkhoven@live.nl>
2014-10-29 09:18:21 +0100

    stuff with testing and reflection and stuff and more stuff

--- src/tests/MoveTest.java
+++ src/tests/MoveTest.java
@@ -1,6 +1,10 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import model.Board;
 import model.Move;
@@ -9,18 +13,16 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.internal.runners.TestMethod;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.junit.runners.Suite;
 
 import utils.Piece;
 import utils.TempPiece;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-@RunWith(Suite.class)
+//@RunWith(Suite.class)
 public class MoveTest {
 
 	Board board;
@@ -52,27 +54,74 @@ public class MoveTest {
 	public void tearDown() throws Exception {
 		board = null;
 	}
+// TODO FIX IT!
+	private final boolean methodTempFieldEquals(TempPiece tP, Piece p) {
+		boolean value = false;
+		try {
+			Method testMethod = Move.class.getDeclaredMethod("tempFieldEquals",
+					new Class[] { TempPiece.class, Piece.class });
+			testMethod.setAccessible(true);
+			value = (boolean) testMethod.invoke(Move.class.newInstance(), new Object[] { tP, p });
+		} catch (NoSuchMethodException | SecurityException
+				| IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | InstantiationException e) {
+			System.err
+					.println("Testing error, try failed @MoveTest.methodTempFieldEquals");
+			e.printStackTrace();
+		}
+		return value;
+	}
 
 	@Test
 	public final void testTempFieldEquals() {
-		try {
-			Method testMethod = Move.class.getDeclaredMethod("tempFieldEquals",
-					new Class[] { Piece.class, TempPiece.class });
-			testMethod.setAccessible(true);
-			testMethod.invoke(null, new Object[] { TempPiece.EMPTY, null });
-		} catch (NoSuchMethodException | SecurityException
-				| IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// Testing true values
+//		assertEquals(true, methodTempFieldEquals(TempPiece.EMPTY, null));
+		for (int i = 0; i < Piece.values().length; i++) {
+			if (Piece.values()[i].toString().contains("WHITE")) {
+				assertEquals(
+						true,
+						methodTempFieldEquals(TempPiece.WHITE,
+								Piece.values()[i]));
+			} else if (Piece.values()[i].toString().contains("BLACK")) {
+				assertEquals(
+						true,
+						methodTempFieldEquals(TempPiece.BLACK,
+								Piece.values()[i]));
+			} else {
+				System.err
+						.println("Error with toString().contains() @MoveTest.testTempFieldEquals");
+			}
 		}
+
+		// Testing false values
+		for (int i = 0; i < Piece.values().length; i++) {
+			if (Piece.values()[i].toString().contains("BLACK")) {
+				assertEquals(
+						false,
+						methodTempFieldEquals(TempPiece.WHITE,
+								Piece.values()[i]));
+			} else if (Piece.values()[i].toString().contains("WHITE")) {
+				assertEquals(
+						false,
+						methodTempFieldEquals(TempPiece.BLACK,
+								Piece.values()[i]));
+			} else {
+				System.err
+						.println("Error with toString().contains() @MoveTest.testTempFieldEquals");
+			}
+			assertEquals(false,
+					methodTempFieldEquals(TempPiece.EMPTY, Piece.values()[i]));
+		}
+
 	}
 
+	@Ignore
 	@Test
 	public final void testMove() {
 		fail("Not yet implemented"); // TODO
 	}
 
+	@Ignore
 	@Test
 	public final void testIsValid() {
 		fail("Not yet implemented"); // TODO
---
+++ src/tests/PrutTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import utils.Piece;
+
+
+public class PrutTest {
+	
+	public static void main(String[] args) {
+		System.out.println("-->   " + Piece.WHITE_BISHOP.toString().contains("white"));
+	}
+
+}
