1874985a3cc0f112ce69953c20ee1f246f43cb51
reycs <rubenhaasjes@gmail.com>
2014-11-04 10:56:41 +0100

    push for tim

--- src/controllers/I2CHandler.java
+++ src/controllers/I2CHandler.java
@@ -7,23 +7,57 @@ import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 import com.pi4j.wiringpi.Gpio;
 
-public class I2CHandler {
+public class I2CHandler extends Thread {
+	
+	private I2CDevice[] dataLines;
 	
 	public I2CHandler () throws IOException {
-		 System.out.println("I2C Reader [Test]");
-	     final I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
-	    
-	     System.out.println("Get device with id [0x06]");
-	     I2CDevice fpga = bus.getDevice(0x06);
-	     System.out.println(fpga.toString());
-	     
-	     System.out.println("Setting pins to up");
-	     // pins
-	     Gpio.pullUpDnControl(8, Gpio.PUD_UP);     
-	     Gpio.pullUpDnControl(9, Gpio.PUD_UP);   
-	     
-	     System.out.println("Reading data");
-	     int x = fpga.read();
-	     System.out.println(x);
- }
+		System.out.println("i2cSquared loading!");
+	}
+
+	/**
+	 * Gets called when thread get started
+	 */
+	public void run() {
+		try {
+			init();
+		} catch (IOException e) {
+			System.out.println("[Error] Initializing addresses");
+		} 
+		// after it has initialized
+	}
+	
+	/**
+	 * Initializes the bus and data addresses
+	 * @throws IOException of it couldn't instantiate the bus or
+	 * read the devices
+	 */
+	private void init() throws IOException {
+		final I2CBus bus = I2CFactory.getInstance(I2CBus.BUS_1);
+		
+		dataLines = new I2CDevice[32];
+		
+		// initialize the data lines
+		for (int i = 0; i < 32; i++) {
+			dataLines[i] = bus.getDevice(i + 1);
+		}
+		
+		Gpio.pullUpDnControl(8, Gpio.PUD_UP);     
+		Gpio.pullUpDnControl(9, Gpio.PUD_UP);  
+	}
+	
+	/**
+	 * Reads the data from the 32 selected adress lines
+	 * @return returns the read data
+	 * @throws IOException
+	 */
+	private int[] readData() throws IOException {
+		int[] data = new int[32];
+		for (int i = 0; i < 32; i++) {
+			data[i] = dataLines[i].read();
+		}
+		return data;
+	}
+	
+	
 }
